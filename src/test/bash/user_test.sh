f13c722e14c9e7c76cab0ec2d6bf83177268ef92
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-09 23:10:52 +0300

    tests

--- src/test/bash/user_test.sh
+++ src/test/bash/user_test.sh
@@ -11,17 +11,61 @@ if ! /usr/local/bin/bash -n "${SCRIPT}"; then
 
 STDERR="$(mktemp)"
 
+# arguments
+
+:> "${STDERR}"
 "${SCRIPT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
 
+:> "${STDERR}"
 "${SCRIPT}" '' 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
 
+:> "${STDERR}"
 "${SCRIPT}" '' '' '' 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+#
+
+:> "${STDERR}"
+"${SCRIPT}" '' '' 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No token!'
+
+GITHUBX_PAT='foo'
+
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_PAT}" '' 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No dst!'
+
+:> "${STDERR}"
+GITHUBX_DST="$(mktemp -d)"
+"${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" is not a file!"
+rm -rf "${GITHUBX_DST}"
+
+:> "${STDERR}"
+GITHUBX_DST="$(mktemp)"
+rm "${GITHUBX_DST}"
+ln -s "${GITHUBX_DST}" "${GITHUBX_DST}"
+"${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" is a symlink!"
+rm "${GITHUBX_DST}"
+
+:> "${STDERR}"
+GITHUBX_DST="$(mktemp)"
+"${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" exists!"
+rm "${GITHUBX_DST}"
+
+#
 
 echo 'Not implemented!'; exit 1 # todo
 
