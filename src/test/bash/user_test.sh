b9b4c84ff84e5d7a240e68e4def93d70d3fbadf7
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-09 23:03:19 +0300

    tests

---
+++ src/test/bash/user_test.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/bash
+
+SCRIPT='src/main/bash/user.sh'
+
+echo "Running test of \"${SCRIPT}\"..."
+
+. $asserts/files/execs.sh "${SCRIPT}"
+
+if ! /usr/local/bin/bash -n "${SCRIPT}"; then
+ echo "\"${SCRIPT}\" has wrong syntax!" >&2; exit 1; fi
+
+STDERR="$(mktemp)"
+
+"${SCRIPT}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+"${SCRIPT}" '' 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+"${SCRIPT}" '' '' '' 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+
+echo 'Not implemented!'; exit 1 # todo
+
+rm "${STDERR}"
