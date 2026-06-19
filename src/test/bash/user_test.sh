3b902e83ec1bd6bdaacaf9e02c58dd5a405c8b97
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-19 23:00:14 +0300

    tests

--- src/test/bash/user_test.sh
+++ src/test/bash/user_test.sh
@@ -12,77 +12,83 @@ if ! /usr/local/bin/bash -n "${SCRIPT}"; then
 STDOUT="$(mktemp)"
 STDERR="$(mktemp)"
 
-# arguments
+#
 
 :> "${STDOUT}"
 :> "${STDERR}"
 "${SCRIPT}" >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+. $asserts/files/equals.sh "${STDERR}" $'Wrong arguments!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 "${SCRIPT}" '' >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+. $asserts/files/equals.sh "${STDERR}" $'Wrong arguments!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
 "${SCRIPT}" '' '' '' >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong arguments!'
+. $asserts/files/equals.sh "${STDERR}" $'Wrong arguments!\n'
 
 #
 
 :> "${STDOUT}"
 :> "${STDERR}"
-"${SCRIPT}" '' '' >"${STDOUT}" 2>"${STDERR}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
-. $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No token!'
-
-GITHUBX_PAT='foo'
-
-:> "${STDOUT}"
-:> "${STDERR}"
+GITHUBX_PAT=''
 "${SCRIPT}" "${GITHUBX_PAT}" '' >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'No dst!'
+. $asserts/files/equals.sh "${STDERR}" $'No token!\n'
 
 :> "${STDOUT}"
 :> "${STDERR}"
+GITHUBX_PAT='foo'
+GITHUBX_DST=''
+"${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" >"${STDOUT}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" $'No dst!\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='foo'
 GITHUBX_DST="$(mktemp -d)"
 "${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" is not a file!"
-rm -rf "${GITHUBX_DST}"
+. $asserts/files/equals.sh "${STDERR}" "\"${GITHUBX_DST}\" is not a file!"$'\n'
+rm -r "${GITHUBX_DST}"
 
 :> "${STDOUT}"
 :> "${STDERR}"
+GITHUBX_PAT='foo'
 GITHUBX_DST="$(mktemp)"
 rm "${GITHUBX_DST}"
 ln -s "${GITHUBX_DST}" "${GITHUBX_DST}"
 "${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" is a symlink!"
-rm "${GITHUBX_DST}"
+. $asserts/files/equals.sh "${STDERR}" "\"${GITHUBX_DST}\" is a symlink!"$'\n'
+rm -r "${GITHUBX_DST}"
 
 :> "${STDOUT}"
 :> "${STDERR}"
+GITHUBX_PAT='foo'
 GITHUBX_DST="$(mktemp)"
 "${SCRIPT}" "${GITHUBX_PAT}" "${GITHUBX_DST}" >"${STDOUT}" 2>"${STDERR}"
 . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" "\"${GITHUBX_DST}\" exists!"
-rm "${GITHUBX_DST}"
+. $asserts/files/equals.sh "${STDERR}" "\"${GITHUBX_DST}\" exists!"$'\n'
+rm -r "${GITHUBX_DST}"
 
 #
+
+echo 'Not implemented!'; exit 1 # todo
 
 :> "${STDOUT}"
 :> "${STDERR}"
