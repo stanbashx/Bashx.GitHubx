c74e3566ee613dff1a6b8cf9752b359cf8c27c12
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 12:47:43 +0300

    tests

--- src/test/bash/user_test.sh
+++ src/test/bash/user_test.sh
@@ -53,7 +53,7 @@ GITHUBX_DST=''
 "${SCRIPT}" "${GITHUBX_PAT_SRC}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Wrong token!\n'
+. $asserts/files/equals.sh "${STDERR}" $'Token is unset!\n'
 
 VALUES=('0' '-')
 for VALUE in "${VALUES[@]}"; do
