7022995ff923dc2c97fecc0ce4a1e90b975e52a1
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-04 22:01:35 +0300

    tests

--- src/test/bash/release_test.sh
+++ src/test/bash/release_test.sh
@@ -44,6 +44,130 @@ STDERR="$(mktemp)"
 
 #
 
+GITHUBX_REP_OWNER=''
+GITHUBX_REP_NAME=''
+GITHUBX_PAT_SRC=''
+GITHUBX_RELEASE_SHA=''
+GITHUBX_RELEASE_VERSION=''
+GITHUBX_RELEASE_MESSAGE=''
+GITHUBX_IS_PRERELEASE=''
+GITHUBX_DST=''
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No repository owner!'$'\n'
+
+GITHUBX_REP_OWNER='foo'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No repository name!'$'\n'
+
+GITHUBX_REP_NAME='bar'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong token!'$'\n'
+
+GITHUBX_PAT_SRC='GITHUBX_PAT'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Token is unset!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No token!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No release SHA!'$'\n'
+
+GITHUBX_RELEASE_SHA='foo'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong release SHA!'$'\n'
+
+GITHUBX_RELEASE_SHA='a085a56c2593123dfabbaf2ca08a0743e66b356f'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No release version!'$'\n'
+
+GITHUBX_RELEASE_VERSION='test/version'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No release message!'$'\n'
+
+GITHUBX_RELEASE_MESSAGE='test message'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong prerelease!'$'\n'
+
+GITHUBX_IS_PRERELEASE='true'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_SHA}" "${GITHUBX_RELEASE_VERSION}" "${GITHUBX_RELEASE_MESSAGE}" "${GITHUBX_IS_PRERELEASE}" "${GITHUBX_DST}" \
+ > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No dst!'$'\n'
+
+#
+
 echo 'Not implemented!'; exit 1 # todo
 
 #
