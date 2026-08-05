c3c1a39d7637971a2e5f09665bc0cdb27e549d65
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-05 21:21:46 +0300

    tests

--- src/test/bash/releases/upload_test.sh
+++ src/test/bash/releases/upload_test.sh
@@ -19,7 +19,23 @@ STDERR="$(mktemp)"
 "${SCRIPT}" > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
-. $asserts/files/equals.sh "${STDERR}" $'Wrong arguments!\n'
+. $asserts/files/equals.sh "${STDERR}" 'Wrong arguments!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" '' '' '' '' '' '' > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong arguments!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" '' '' '' '' '' '' '' '' > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong arguments!'$'\n'
+
+#
 
 echo 'Not implemented!'; exit 1 # todo
 
