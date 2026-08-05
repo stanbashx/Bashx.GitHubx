73de80102f5aeeb4ae0eca8a6c7f145b6bb6c0f9
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-05 21:34:26 +0300

    tests

--- src/test/bash/releases/upload_test.sh
+++ src/test/bash/releases/upload_test.sh
@@ -39,6 +39,7 @@ STDERR="$(mktemp)"
 
 GITHUBX_REP_OWNER=''
 GITHUBX_REP_NAME=''
+unset GITHUBX_PAT
 GITHUBX_PAT_SRC=''
 GITHUBX_RELEASE_ID=''
 GITHUBX_ASSET_PATH=''
@@ -88,6 +89,70 @@ GITHUBX_PAT='' \
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
 . $asserts/files/empty.sh "${STDOUT}"
 . $asserts/files/equals.sh "${STDERR}" 'No token!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No release ID!'$'\n'
+
+GITHUBX_RELEASE_ID='a'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong release ID!'$'\n'
+
+GITHUBX_RELEASE_ID='1'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No asset path!'$'\n'
+
+GITHUBX_ASSET_PATH="$(mktemp)"
+printf '%s' 'foobarbaz' > "${GITHUBX_ASSET_PATH}"
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No asset name!'$'\n'
+
+GITHUBX_ASSET_NAME='test_asset.txt'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No dst!'$'\n'
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_DST="$(mktemp)"
+rm "${GITHUBX_DST}"
+PATH="${mocks}/curl/bin:${PATH}" \
+ MOCKS_CURL_EXIT_CODE=1 \
+ GITHUBX_PAT='42' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Request error!'$'\n'
+rm -f "${GITHUBX_DST}"
 
 #
 
