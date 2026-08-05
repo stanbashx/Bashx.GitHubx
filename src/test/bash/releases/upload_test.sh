41df3ae5163c528fb7a59c8baca59ec4a59b6d9a
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-05 21:24:43 +0300

    tests

--- src/test/bash/releases/upload_test.sh
+++ src/test/bash/releases/upload_test.sh
@@ -37,6 +37,60 @@ STDERR="$(mktemp)"
 
 #
 
+GITHUBX_REP_OWNER=''
+GITHUBX_REP_NAME=''
+GITHUBX_PAT_SRC=''
+GITHUBX_RELEASE_ID=''
+GITHUBX_ASSET_PATH=''
+GITHUBX_ASSET_NAME=''
+GITHUBX_DST=''
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No repository owner!'$'\n'
+
+GITHUBX_REP_OWNER='foo'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No repository name!'$'\n'
+
+GITHUBX_REP_NAME='bar'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Wrong token!'$'\n'
+
+GITHUBX_PAT_SRC='GITHUBX_PAT'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'Token is unset!'$'\n'
+
+:> "${STDOUT}"
+:> "${STDERR}"
+GITHUBX_PAT='' \
+ "${SCRIPT}" "${GITHUBX_REP_OWNER}" "${GITHUBX_REP_NAME}" "${GITHUBX_PAT_SRC}" "${GITHUBX_RELEASE_ID}" "${GITHUBX_ASSET_PATH}" "${GITHUBX_ASSET_NAME}" "${GITHUBX_DST}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" 'No token!'$'\n'
+
+#
+
 echo 'Not implemented!'; exit 1 # todo
 
 #
